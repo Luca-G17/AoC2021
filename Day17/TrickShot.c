ee76c8eb5862402d130edf2243f371121f2cca16
Luca <lucagough7@gmail.com>
2021-12-17 12:51:15 +0000

    Cleanup

--- Day17/TrickShot.c
+++ Day17/TrickShot.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-
-#define X_VEL_MAX 1000
-#define Y_VEL_MAX 1000
+// Increase these until the answer doesnt change
+#define X_VEL_MAX 200
+#define Y_VEL_MAX 200
 
 typedef struct Target{
     int x1;
@@ -61,7 +61,6 @@ int simulateProbeTrajectory(int initXvel, int initYvel, target* t, int yMax, int
         yVel--;
         yCoords[n++] = y;
         if (y >= t->y1 && y <= t->y2 && x >= t->x1 && x <= t->x2){
-            // Probe landed inside trench
             *trenchCount += 1;
             localYMax = arrMax(n, yCoords);
             tracking = false;
@@ -75,14 +74,12 @@ int simulateManyProbeTraj(target* t, int *tC){
     for (int xVel = -X_VEL_MAX; xVel < X_VEL_MAX; xVel++){
         for (int yVel = -Y_VEL_MAX; yVel < Y_VEL_MAX; yVel++){
             yMax = simulateProbeTrajectory(xVel, yVel, t, yMax, tC);
-            // printf("XVel: %d, YVel: %d\n", xVel, yVel);
         }
     }
     return yMax;
 }
 int main(int n, char *args[n]){
     target *targetArea = readFile(args[1]);
-    // printf("((%d -> %d), (%d -> %d))\n", targetArea->x1, targetArea->x2, targetArea->y1, targetArea->y2);
     int trenchCount = 0;
     int y = simulateManyProbeTraj(targetArea, &trenchCount);
     printf("The maximum Y coordinate for a probe that lands in the trench is %d\n", y);
